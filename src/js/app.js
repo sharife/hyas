5d00a61f166605173eeb2aad6ba87f4aac6a8b27
Jchinja <chinjm@umich.edu>
2022-12-03 12:56:55 -0500

    maybe fix content policy

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,24 @@
 
 window.showText = showText;
 
+
+var collection1 = document.getElementById("Index one*").addEventListener("click", showText("Index one"))
+var collection2 = document.getElementById("Index 2*").addEventListener("click", showText("Index 2"))
+
+
+
+// var collection = document.getElementsByClassName("content-button");
+
+// console.log(collection)
+
+// for (var i=0; i<collection.length;i++){
+
+//       // collection[i].addEventListener("click", showText(collection[i].id.slice(0,-1)))
+//       collection[i].onclick = showText(collection[i].id.slice(0,-1))
+// }
+// console.log(collection)
+
+
 function showText(temp) {
       var x = document.getElementById(temp);
       var arrayOfElements= document.getElementsByClassName("story")
@@ -13,3 +31,15 @@ function showText(temp) {
       x.style.display = "none";
       }
 }
+
+// JS Goes here - ES6 supported
+if (window.netlifyIdentity) {
+      window.netlifyIdentity.on("init", (user) => {
+        if (!user) {
+          window.netlifyIdentity.on("login", () => {
+            document.location.href = "/admin/";
+          });
+        }
+      });
+    }
+    
--- static/assets/bundle.js
+++ static/assets/bundle.js
@@ -1,1 +1,32 @@
-window.showText=function(e){for(var n=document.getElementById(e),t=document.getElementsByClassName("story"),l=0;l<t.length;l++)t[l].style.display="none";"none"===n.style.display?n.style.display="block":n.style.display="none"};
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (function() { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/app.js":
+/*!***********************!*\
+  !*** ./src/js/app.js ***!
+  \***********************/
+/***/ (function() {
+
+eval("\nwindow.showText = showText;\n\n\nvar collection1 = document.getElementById(\"Index one*\").addEventListener(\"click\", showText(\"Index one\"))\nvar collection2 = document.getElementById(\"Index 2*\").addEventListener(\"click\", showText(\"Index 2\"))\n\n\n\n// var collection = document.getElementsByClassName(\"content-button\");\n\n// console.log(collection)\n\n// for (var i=0; i<collection.length;i++){\n\n//       // collection[i].addEventListener(\"click\", showText(collection[i].id.slice(0,-1)))\n//       collection[i].onclick = showText(collection[i].id.slice(0,-1))\n// }\n// console.log(collection)\n\n\nfunction showText(temp) {\n      var x = document.getElementById(temp);\n      var arrayOfElements= document.getElementsByClassName(\"story\")\n      for (var i=0; i<arrayOfElements.length;i++){\n         arrayOfElements[i].style.display='none';\n      }\n      if (x.style.display === \"none\") {\n      x.style.display = \"block\";\n      } else {\n      x.style.display = \"none\";\n      }\n}\n\n// JS Goes here - ES6 supported\nif (window.netlifyIdentity) {\n      window.netlifyIdentity.on(\"init\", (user) => {\n        if (!user) {\n          window.netlifyIdentity.on(\"login\", () => {\n            document.location.href = \"/admin/\";\n          });\n        }\n      });\n    }\n    \n\n//# sourceURL=webpack://gethyas/./src/js/app.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/js/app.js"]();
+/******/ 	
+/******/ })()
+;
